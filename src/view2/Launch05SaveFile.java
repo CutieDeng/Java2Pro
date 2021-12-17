4bac7e57d9a8e0203fb7413f43576b7ee1f4d517
ChristinaLJC <1315552992@qq.com>
2021-12-17 22:56:44 +0800

    update Launch05SaveFile.java
    - add new functions related to files

--- src/view2/Launch05SaveFile.java
+++ src/view2/Launch05SaveFile.java
@@ -3,47 +3,167 @@ package view2;
 import com.sun.javafx.robot.impl.FXRobotHelper;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
+import java.io.*;
+import java.util.List;
 
 public class Launch05SaveFile extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Button button = new Button("TEST");
+        AnchorPane anchorPane = new AnchorPane();
 
-        button.setOnAction(this::handExportDateAction);
+        VBox vBox = new VBox(10);
 
-        primaryStage.setScene(new Scene(new AnchorPane(button)));
-        primaryStage.setHeight(630);
-        primaryStage.setWidth(1050);
+        Button button1 = new Button("one file");
+        Button button2 = new Button("multi files");
+        Button button3 = new Button("open file");
+        Button button4 = new Button("save file");
+        TextArea textArea = new TextArea();
+        textArea.setWrapText(true);//设置自动换行
+
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Stage stage = new Stage();
+                FileChooser fileChooser = new FileChooser();
+
+                fileChooser.setTitle("choose one file");
+
+                //默认路径
+                fileChooser.setInitialDirectory(new File("D:" + File.separator));
+
+                //设置选择的文件的扩展名
+                fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("txt type", "*.txt"));
+
+                //返回用户选中的文件的路径，注意，如果用户不选，则会返回null
+                File file = fileChooser.showOpenDialog(stage);
+
+                if (file != null) System.out.println(file.getAbsoluteFile());
+            }
+        });
+
+        button2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Stage stage = new Stage();
+                FileChooser fileChooser = new FileChooser();
+
+                fileChooser.setTitle("choose one file");
+
+                //默认路径
+                fileChooser.setInitialDirectory(new File("D:" + File.separator));
+
+                //设置选择的文件的扩展名
+                fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("txt type", "*.txt"));
+
+                //返回用户选中的文件的路径，注意，如果用户不选，则会返回null
+                List<File> files = fileChooser.showOpenMultipleDialog(stage);
+
+                if (files != null) {
+                    for (File f : files) {
+                        System.out.println(f.getAbsoluteFile());
+                    }
+                }
+
+            }
+        });
+
+        button3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Stage stage = new Stage();
+                FileChooser fileChooser = new FileChooser();
+
+                fileChooser.setTitle("open one file");
+
+                //默认路径
+                fileChooser.setInitialDirectory(new File("D:" + File.separator));
+
+                //设置选择的文件的扩展名
+                fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("txt type", "*.txt"));
+
+                //返回用户选中的文件的路径，注意，如果用户不选，则会返回null
+                File file = fileChooser.showOpenDialog(stage);
+
+                if (file != null) {
+                    try {
+                        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+                        String s = null;
+                        while ((s = bufferedReader.readLine()) != null) {
+                            textArea.appendText(s);
+                        }
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        button4.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Stage stage = new Stage();
+                FileChooser fileChooser = new FileChooser();
+
+                fileChooser.setTitle("save one file");
+
+                //默认文件名
+                fileChooser.setInitialFileName("default");
+
+                //默认路径
+                fileChooser.setInitialDirectory(new File("D:" + File.separator));
+
+                //设置选择的文件的扩展名
+                fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("txt type", "*.txt"));
+
+                //返回用户选中的文件的路径，注意，如果用户不选，则会返回null
+                File file = fileChooser.showSaveDialog(stage);
+
+                if (file == null) return;
+
+
+                try {
+                    FileOutputStream fos = new FileOutputStream(file);
+                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fos);
+
+                    outputStreamWriter.write(textArea.getText());
+
+                    outputStreamWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+//                try {
+//                    file.createNewFile();//如果保存的文件没有数据，则需要这句话。否则，不需要
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        });
+
+
+        anchorPane.getChildren().add(vBox);
+        vBox.getChildren().addAll(button1, button2, button3, button4, textArea);
+
+        primaryStage.setScene(new Scene(anchorPane));
+        primaryStage.setHeight(600);
+        primaryStage.setWidth(800);
         primaryStage.show();
-    }
-
-    protected void handExportDateAction(ActionEvent event) {
-        // ShowDialog.showConfirmDialog(FXRobotHelper.getStages().get(0),
-        // "是否导出数据到txt？", "信息");
-        FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
-        fileChooser.getExtensionFilters().add(extFilter);
-        Stage s = new Stage();
-        File file = fileChooser.showSaveDialog(s);
-        if (file == null)
-            return;
-        if(file.exists()){//文件已存在，则删除覆盖文件
-            file.delete();
-        }
-        String exportFilePath = file.getAbsolutePath();
-        System.out.println("导出文件的路径" + exportFilePath);
-
-
     }
 }
